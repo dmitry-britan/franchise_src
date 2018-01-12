7bbcd03d7d975f5bb1f6d998dff060877d468e1c
Dmitry Britan <dmitry.britan@gmail.com>
2018-01-12 23:03:38 +0200

    added login page

--- src/js/module/form-validation.js
+++ src/js/module/form-validation.js
@@ -1,3 +1,4 @@
+// @include('validate/loginForm.js')
 // @include('validate/askForm.js')
 // @include('validate/callbackForm.js')
 // @include('validate/registrationForm.js')
---
+++ src/js/module/validate/loginForm.js
@@ -0,0 +1,34 @@
+//
+// Валидация формы "Задать вопрос"
+// =================================================================
+let validateFormLogin = {
+	rules: {
+		email: {
+			required: true,
+			email: true,
+		},
+		password: {
+			required: true,
+		},
+	},
+	messages: {
+		email: {
+			email: 'Введите корректный e-mail адрес',
+			required: 'Введите Ваш e-mail',
+		},
+		password: {
+			required: 'Введите Ваш пароль',
+		},
+	},
+	submitHandler: (form) => {
+		// ////////////////////
+		//  AJAX CODE GOES HERE
+		// ////////////////////
+		form.reset();
+	},
+	focusCleanup: true,
+	focusInvalid: false,
+};
+
+// Login Form
+$('.js-form-login').validate(validateFormLogin);
